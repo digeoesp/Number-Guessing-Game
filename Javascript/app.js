92b8634853cb2252c4278a93d5ea4b0acf6ed7df
digeoesp <diegocrypto@outlook.com>
2021-01-26 15:41:49 -0800

    input displays as array q q q1 q! wq!

--- Javascript/app.js
+++ Javascript/app.js
@@ -4,30 +4,30 @@ let input = document.getElementById('inBox')
 let number = [Math.floor(Math.random() * 100)]
 let inputAnswer = document.getElementById('inputAnswers')
 let submit = document.getElementById('submit')
+let answers = []
 
+// easyButton.addEventListener('click', ()=> {
+//     console.log('easy button');
+// })
+// console.log(easyButton)
 
-easyButton.addEventListener('click', ()=> {
-    console.log('easy button');
-})
-console.log(easyButton)
-
-hardButton.addEventListener('click', ()=> {
-    console.log('hard button');
-})
-console.log(hardButton)
+// hardButton.addEventListener('click', ()=> {
+//     console.log('hard button');
+// })
+// console.log(hardButton)
 //input MDN
 //once submitt button is clicked i want it to display in inputAnswer 
 submit.addEventListener('click', ()=>{
-    answer = input.value;
-    inputAnswers.innerHTML = answer;
-    
-
-    
+    answers.push(input.value);
+    inputAnswers.innerHTML = (`${answers},`);
+    input.value = ''
 })
     
-let answer = (inputAnswers.innerHTML )
-console.log(answer)
     
+
+
+    
+
     
 
 
