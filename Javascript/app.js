a64e41dbc5e692d42697d91014821dac13bad868
digeoesp <diegocrypto@outlook.com>
2021-01-26 21:37:32 -0800

    added code for number of attempts

--- Javascript/app.js
+++ Javascript/app.js
@@ -5,25 +5,37 @@ let number = [Math.floor(Math.random() * 100)]
 let inputAnswer = document.getElementById('inputAnswers')
 let submit = document.getElementById('submit')
 let answers = []
+let numberOfAttempts = document.getElementById('numberOfAttempts')
+let attempts = []
+let count = 0
 
-// easyButton.addEventListener('click', ()=> {
-//     console.log('easy button');
-// })
-// console.log(easyButton)
 
-// hardButton.addEventListener('click', ()=> {
-//     console.log('hard button');
-// })
-// console.log(hardButton)
+
+easyButton.addEventListener('click', ()=> {
+    console.log('easy button');
+})
+console.log(easyButton)
+
+hardButton.addEventListener('click', ()=> {
+    console.log('hard button');
+})
+console.log(hardButton)
 //input MDN
 //once submitt button is clicked i want it to display in inputAnswer 
+
 submit.addEventListener('click', ()=>{
+    numberOfAttempts.value = count;
     answers.push(input.value);
+    count++;
+    console.log(count)
+    numberOfAttempts.append(count)
     inputAnswers.innerHTML = (`${answers},`);
-    input.value = ''
+    input.value = '';
 })
     
-    
+
+
+
 
 
     
